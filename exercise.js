3972d556a301cdb31839866b27b7dd11a0b63507
0812728853 <145583955+0812728853@users.noreply.github.com>
2023-10-06 13:41:34 +0700

    Ex-09

---
+++ exercise.js
@@ -0,0 +1,14 @@
+// Diện tích
+// // let r=Number(prompt ("Nhập bán kính"));
+// // let s=3.14*r*r;
+// // document.write(s);
+
+
+
+
+
+// // Chu vi
+// let r=Number(prompt ("Nhập bán kính"));
+// let c=2*r*3.14;
+// document.write(c) ;
+
